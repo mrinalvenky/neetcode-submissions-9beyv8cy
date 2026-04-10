77866e83f067bd6314f0ee89b6dbf43fe921fc8c
Mrinal Venkatesh <mrinalvenky@gmail.com>
2026-04-09 18:42:17 -0700

    Add: graph - submission-2

---
+++ Data Structures & Algorithms/graph/submission-2.java
@@ -0,0 +1,42 @@
+class Graph {
+    Map<Integer, List<Integer>> adjList;
+    public Graph() {
+        adjList = new HashMap<>();
+    }
+
+    public void addEdge(int src, int dst) {
+        adjList.computeIfAbsent(src, k -> new ArrayList<>()).add(dst);
+    }
+
+    public boolean removeEdge(int src, int dst) {
+        if(!adjList.containsKey(src)) {
+            return false;
+        }
+        return adjList.get(src).remove(Integer.valueOf(dst));
+    }
+
+    public boolean hasPath(int src, int dst) {
+        return hasPathBFS(src, dst);
+    }
+
+    private boolean hasPathBFS(int src, int dst) {
+        Set<Integer> visited = new HashSet<>();
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.offer(src);
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            if (node == dst) {
+                return true;
+            }
+            visited.add(node);
+            for (int nei: adjList.getOrDefault(node, Collections.emptyList())) {
+                if (!visited.contains(nei)) {
+                    visited.add(nei);
+                    queue.offer(nei);
+                }
+            }
+        }
+        return false;
+    }
+}
