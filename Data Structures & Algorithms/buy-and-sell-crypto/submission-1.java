cbc4077d82807754db4e1b77f5d305a9cdec0865
Mrinal Venkatesh <mrinalvenky@gmail.com>
2026-04-11 10:29:01 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        /*int l = 0, maxP = 0;
+        for (int r = 1; r < prices.length ; r++) {
+            if (prices[l] < prices[r]) {
+                int profit = prices[r] - prices[l];
+                maxP = Math.max(maxP, profit);
+            } else {
+                l = r;
+            }
+        }
+        return maxP;*/
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
