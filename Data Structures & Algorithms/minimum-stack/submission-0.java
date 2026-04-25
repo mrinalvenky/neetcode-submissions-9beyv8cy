c98a79f33cccaa6cad5fba12e3ab04aca13b241e
Mrinal Venkatesh <mrinalvenky@gmail.com>
2026-04-24 19:33:40 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,26 @@
+class MinStack {
+    Deque<Integer> stack, minStack;
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        int minTop = !minStack.isEmpty() ? Math.min(minStack.peek(), val) : val;
+        minStack.push(minTop);
+    }
+    
+    public void pop() {
+        minStack.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
