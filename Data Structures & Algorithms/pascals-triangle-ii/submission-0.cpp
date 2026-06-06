4fb057169783e4c612bc02c173698833c1acf34e
Mrinal Venkatesh <mrinalvenky@gmail.com>
2026-06-06 12:25:03 -0700

    Add: pascals-triangle-ii - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> pascal(rowIndex + 1);
+        for (int i = 0; i <= rowIndex; i++) {
+            pascal[i].resize(i + 1);
+            pascal[i][0] = pascal[i][i] = 1;
+            for (int j = 1 ; j < i ; j++) {
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+            }
+        }
+        return pascal.back();
+    }
+};
